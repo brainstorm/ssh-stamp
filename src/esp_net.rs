750df6a2628ef5a3da32e6a3d755689521b4160f
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2024-09-06 22:47:31 +1000

    Runs without crashing, 'medium-ethernet' required for this to work

--- src/esp_net.rs
+++ src/esp_net.rs
@@ -34,7 +34,7 @@ use esp_wifi::{
     },
     EspWifiInitFor,
 };
-use static_cell::make_static;
+//use static_cell::make_static;
 
 // When you are okay with using a nightly compiler it's better to use https://docs.rs/static_cell/2.1.0/static_cell/macro.make_static.html
 macro_rules! mk_static {
