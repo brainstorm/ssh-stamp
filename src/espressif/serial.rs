6b0e2478c35f9d1075a646d001356b2bbf303f3e
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2025-04-10 16:39:36 +1000

    Follow on serial software interrupt implementation.
    Co-authored-by: Angus Gratton <projectgus@users.noreply.github.com>

--- src/espressif/serial.rs
+++ src/espressif/serial.rs
@@ -15,57 +15,57 @@
 use embassy_executor::Spawner;
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, pipe::Pipe};
 use embassy_futures::select::select;
+use embedded_io_async;
 use esp_hal::Async;
-use esp_hal::uart::Uart;
+use esp_hal::uart::{ Uart, UartRx, UartTx };
 
 const RD_BUF_SZ: usize = 512;
 const WR_BUF_SZ: usize = 256;
 
 struct BufferedUart<'a> {
-    uart: Uart<'a, Async>,
+    uart_rx: UartRx<'a, Async>,
+    uart_tx: UartTx<'a, Async>,
     from_uart: Pipe<CriticalSectionRawMutex, RD_BUF_SZ>,
     to_uart: Pipe<CriticalSectionRawMutex, WR_BUF_SZ>,
 }
 
-impl BufferedUart<'static> {
-    pub fn uart_init(spawner: Spawner) -> Self {
-
-
+impl<'a> BufferedUart<'a> {
+    pub fn new(uart: impl Into<Uart<'a, Async>>) -> Self {
+        let (mut uart_rx, mut uart_tx) = self.uart.into().split();
+        return BufferedUart { uart_rx, uart_tx, from_uart: Pipe::new(), to_uart: Pipe::new() }
     }
 
     // Call this from inside the embassy Task
-    pub async fn run(&mut self) {
-        let (mut uart_rx, mut uart_tx) = self.uart.split();
+    pub async fn run(&self) {
         let mut uart_rx_buf = [0u8; 128];
         let mut uart_tx_buf = [0u8; 128];
         loop {
             let rd_from = async { 
                 loop {
-                    let n = uart_rx.read(&mut uart_rx_buf).await.unwrap();
-                    self.from_uart.write_all(&uart_rx_buf[:n]).await.unwrap();
+                    let n = self.uart_rx.read_async(&mut uart_rx_buf).await.unwrap();
+                    self.from_uart.write_all(&uart_rx_buf[..n]).await;
                 }
             };
             let rd_to = async {
                 loop {
-                   self.to_uart.read(&mut uart_tx_buf);
-                   uart_tx.write_all()
+                   let n = self.to_uart.read(&mut uart_tx_buf).await;
+                   self.uart_tx.write_async(&uart_tx_buf[..n]).await;
                 }
-            }
-            match select(rf_from, rd_to).await {
-    
-            }
+            };
+            
+            select(rd_from, rd_to).await;
         }
     }
     
-    pub async fn read(&mut self, &mut buf: [u8]) -> usize {
+    pub async fn read(&self, &mut buf: [u8]) -> usize {
         self.from_uart.read(buf).await
     }
 
-    pub async fn write(&mut self, &buf: [u8]) {
+    pub async fn write(&self, &buf: [u8]) {
         self.to_uart.write_all(buf).await
     }
 
-    pub fn reconfigure(&mut self, config: Config) {
+    pub fn reconfigure(&self, config: Config) {
         todo!();
     }
 
