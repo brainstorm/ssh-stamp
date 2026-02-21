398e7f5344c90bda06e9cdf82df326772d0222bb
Autofix <209348056+Autofix@users.noreply.github.com>
2026-02-21 12:26:07 +1100

    Remove unused storage/src/esp.rs

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -11,8 +11,6 @@
     feature = "esp32c3",
     feature = "esp32c6"
 ))]
-pub mod esp;
-
 // TODO: When the time comes, generalise the flash so it can be used with all supported targets
 /// [[flash]] is a packet to provide safe access to the Flash storage used by SSH-Stamp
 ///
