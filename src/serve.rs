39772d2568527e9b3912218bd4c3be2fa0bb209d
jubeormk1 <jubeormk1@gmail.com>
2026-03-10 14:32:39 +1100

    fix: Splitting channel fixes issue closing the session on channel EOF
    Before now we where cloning the stdio for the shell channel. We used one stdio to read and the other (stdio2) for writing. This causes some internal issue in sunset and trying to read on a channel that has received an EOF would not return Ok(0) or error.
    
    Splitting the stdio instead of cloning fixes the behavior.
    
    for the shake of exploring the situation I have tried the next combinations:
    
    - cloning stdio, reading from the cloned stdio: No Ok(0)
    - cloning stdio and splitting it. Using split stdio for read/write: No Ok(0)
    
    In summary, when cloned, we loose the Ok(0) expected after the channel has received EOF
    
    This time I have checked the warnings so the CI should be happy ;-)

--- src/serve.rs
+++ src/serve.rs
@@ -271,7 +271,6 @@ pub async fn ssh_disable() -> () {
 
 use crate::espressif::buffered_uart::BufferedUart;
 use crate::serial::serial_bridge;
-use sunset_async::ChanInOut;
 
 pub async fn handle_ssh_client<'a, 'b>(
     uart_buff: &'a BufferedUart,
@@ -284,10 +283,9 @@ pub async fn handle_ssh_client<'a, 'b>(
     match session_type {
         SessionType::Bridge(ch) => {
             info!("Handling bridge session");
-            let stdio: ChanInOut<'_> = ssh_server.stdio(ch).await?;
-            let stdio2 = stdio.clone();
+            let (stdin, stdout) = ssh_server.stdio(ch).await?.split();
             info!("Starting bridge");
-            serial_bridge(stdio, stdio2, uart_buff).await?
+            serial_bridge(stdin, stdout, uart_buff).await?
         }
         #[cfg(feature = "sftp-ota")]
         SessionType::Sftp(ch) => {
