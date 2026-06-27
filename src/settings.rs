6473c5008b1f314f74878cb1edd6527d5a9060e5
brainstorm <brainstorm@nopcode.org>
2026-06-27 10:39:38 +0200

    Concatenate sunset and ssh-stamp versions for more informative bug reports, thanks @mkj for the idea

--- src/settings.rs
+++ src/settings.rs
@@ -13,9 +13,10 @@ use core::net::Ipv4Addr;
 // SSH server settings
 //pub(crate) const MTU: usize = 1536;
 //pub(crate) const PORT: u16 = 22;
-//pub(crate) const SSH_SERVER_ID: &str = "SSH-2.0-ssh-stamp-0.1";
+pub(crate) const SSH_STAMP_IDENT: &str = env!("SSH_STAMP_IDENT");
 pub(crate) const KEY_SLOTS: usize = 1; // TODO: Document whether this a "reasonable default"? Justify why?
-pub const DEFAULT_IP: Ipv4Addr = Ipv4Addr::new(192, 168, 4, 1);
+pub const DEFAULT_IP: Ipv4Addr = Ipv4Addr::new(192, 168, 4, 1); // TODO: Expose this setting via
+                                                                // SSH_STAMP env var?
 
 // WiFi SSID and password character set (alphanumeric)
 pub(crate) const WIFI_PASSWORD_CHARS: &[u8; 62] =
